91b4a639f8c2eb33dc8127302de7ac46e7112e0b
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-03-05 23:04:26 -0500

    Switched quantity selector.

--- components/quantity-selector.php
+++ components/quantity-selector.php
@@ -7,7 +7,7 @@
 ?>
 
 <div class="btn-group" style="width: max-content; height: max-content;">
-    <button onclick="increment('<?= $number_id ?>')" class="btn btn-dark">+</button>
-    <span id="<?= $number_id ?>" class="px-3 py-2"><?= $quantity ?></span>
     <button onclick="decrement('<?= $number_id ?>')" class="btn btn-dark">-</button>
+    <span id="<?= $number_id ?>" class="px-3 py-2"><?= $quantity ?></span>
+    <button onclick="increment('<?= $number_id ?>')" class="btn btn-dark">+</button>
 </div>
